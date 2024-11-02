8454fb70bf88978f6ba6089dc2044dec37fcaafc
Conqr <173382345+NotConqr@users.noreply.github.com>
2024-11-02 17:39:46 +0700

    Create Sever.cpp

---
+++ Sever.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+void wait(int sec);
+
+void wait(int sec) {
+    std::this_thread::sleep_for(std::chrono::seconds(sec));
+}
+
+int main() {
+  for (int i = 0; i < 10; i++) {
+      std::cout << "This File Is From Sever" << "\n" << std::endl;
+  }
+  wait(5)
+}
